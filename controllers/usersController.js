30123c5a4fe0ca098fc91a004f6fe2e3cf6f6a05
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-05 01:21:35 -0300

    agrego tokens para login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const User = require('../Models/usersModel');
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
 
 const createNewUser = async (req, res) => {
   const { correoUser, passwordUser, nickNameUser, countryUser } = req.body;
@@ -18,7 +19,12 @@ const createNewUser = async (req, res) => {
       passwordUser: passwordUser,
       countryUser: countryUser
     })
-    const newUser = await user.save();
+
+    const salt = bcrypt.genSaltSync(10)
+    //aca es donde se encripta la contraseña, agarro el modelo y le inyecto lo que viene de front + el salt
+    user.passwordUser = bcrypt.hashSync(passwordUser, salt )
+    //aca se crea el usuario y se envia al back
+    const newUser = await user.save(); //save es un metodo de express
     res.status(201).json('usuario creado')
   } catch (error) {
     res.status(400).json(error)
