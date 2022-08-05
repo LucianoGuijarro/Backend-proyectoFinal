649c27156a7f98732a1bc6bf5b7711225647525b
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-06 01:31:36 +0200

    agrego endpoint y funcion eliminarCategoria

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const router = express.Router();
-const { agregarCategoria, verCategorias } = require('../controllers/categorias.controller');
+const { agregarCategoria, verCategorias, eliminarCategoria } = require('../controllers/categorias.controller');
 
 router.get('/verCategorias', verCategorias);
 router.post('/agregarCategoria', agregarCategoria);
+router.delete('/eliminarCategoria/:id', eliminarCategoria)
 
 
 
