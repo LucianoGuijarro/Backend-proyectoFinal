c7c5b26c366e1dbb207ce4af6c2bd7d8bc08e2c8
Maxi Alzugaray <maxialz99@hotmail.com>
2022-08-05 23:56:04 -0300

    modifico controlador

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -5,7 +5,12 @@ const login = async (req, res) => {
   const { correoUser, passwordUser } = req.body
   // verificando si existe el usuario en la base de datos
   const user = await User.findOne( {correoUser} )
-  // console.log(emailUser);
+
+  if(user == null){
+    alert("email no existe URA")
+    return res.status(401).json("El Email no existe")
+    
+  }
   console.log(user)
   try {
     if (!user){
