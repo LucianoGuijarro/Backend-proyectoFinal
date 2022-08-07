3b36cd95922223582722d8f63acde3a5e132384b
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-07 18:02:13 +0200

    agregando validacion al password

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const { emailExist } = require('../Helpers/validation')
 
 route.post('/agregarUsuario',
 body('correoUser').not().isEmpty().withMessage("El Email es Requerido").isEmail().withMessage("Ingrese un correo valido").custom(emailExist),
+body('passwordUser').not().isEmpty().withMessage('Este campo no puede estar vacio').matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/),
  createNewUser )
 
  route.get('/verTodos', verTodos);
