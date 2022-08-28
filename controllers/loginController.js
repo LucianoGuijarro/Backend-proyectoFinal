5b94d49e464a02351d3e5e19809dd151ceb4a198
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-08-28 21:20:48 +0200

    arreglando que el error de userSuspendido me devuelva un 401

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,7 +8,7 @@ const loginController = async (req, res) => {
   const { correoUser, passwordUser } = req.body
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+    return res.status(401).json({ errors: errors.array() })
   }
 
   const user = await User.findOne({ correoUser })
