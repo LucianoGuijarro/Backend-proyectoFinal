94bf9c29e425732f794661bce60db58b4e82e0c3
Luciano Ezequiel Guijarro <guijarro.luciano1995@gmail.com>
2022-09-02 19:04:27 +0200

    creo controlador y ruta de eliminarComentario

--- controllers/comentarios.controller.js
+++ controllers/comentarios.controller.js
@@ -29,8 +29,22 @@ const verComentariosPorJuego = async (req, res) => {
         res.status(400).json('No se pudo enviar el comentario')
     }
 }
-
+const eliminarComentario = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const existeComentario = Comentario.find(id);
+        if(existeComentario){
+            res.status(200).json('El comentario fue eliminado con exito')
+            const resultado = await Comentario.findByIdAndRemove(id)
+        }else{
+            return res.status(400).json('El comentario no existe')
+        }
+    } catch (error) {
+        return res.status(400).json(error)
+    }
+}
 module.exports = {
     agregarComentario,
-    verComentariosPorJuego
+    verComentariosPorJuego,
+    eliminarComentario
 }
--- routes/comentarios.js
+++ routes/comentarios.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { agregarComentario, verComentariosPorJuego } = require ('../controllers/comentarios.controller')
+const { agregarComentario, verComentariosPorJuego, eliminarComentario } = require ('../controllers/comentarios.controller')
+const { ensureUser } = require('../middleware/ensureUser');
 
 router.post('/agregarComentario', agregarComentario)
 router.get('/verComentariosPorJuego/:juegoPerteneciente', verComentariosPorJuego)
+router.delete('eliminarComentario',ensureUser, eliminarComentario )
 
 module.exports = router;
